b1716214749446a251e864454aca8f6159c3cfb5
anirudhsai1234 <71549782+anirudhsai1234@users.noreply.github.com>
2020-12-05 15:58:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,9 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+var packageBody,ground;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var left;
-var right;
-var centerbody;
 function preload()
 {
 	helicopterIMG=loadImage("helicopter.png")
@@ -29,6 +26,7 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
+	
 
 	engine = Engine.create();
 	world = engine.world;
@@ -39,12 +37,26 @@ function setup() {
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+ 	World.add(world, ground,);
 
+	 boxPosition=width/2-100;
+	 boxY=610;
+	 boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+	 boxleftSprite.shapeColor="red";
+	 boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+	 World.add(world, boxLeftBody);
+	 boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+	 boxBase.shapeColor="red";
+	 boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+	 World.add(world, boxBottomBody);
+     boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	 boxleftSprite.shapeColor="red";
+	 boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+	 World.add(world, boxRightBody);
 
 	Engine.run(engine);
 
-	centerbody=new Log(400,600,200,PI/2);
+
   
 }
 
@@ -54,7 +66,7 @@ function draw() {
   background(0);
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
-  centerbody.display();
+  
   drawSprites();
  
 }
